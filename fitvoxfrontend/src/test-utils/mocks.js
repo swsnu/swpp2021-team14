f3470b69c26b16e2b53b3a387974bf327ffe05a1
jaeyeonkim99 <jaeyeonkim99@naver.com>
2021-10-29 01:39:33 +0900

    Add Mock Utile, Modify db.json

---
+++ fitvoxfrontend/src/test-utils/mocks.js
@@ -0,0 +1,37 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import * as actionTypes from '../store/actions/actionTypes'
+
+
+const getLoginReducer = jest.fn(
+    initialState =>(state=initialState, action) =>{
+        switch (action.type){
+            default:
+                return {...initialState.login}
+        }
+    }
+)
+
+
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]
+
+export const getMockStore = (initialState)=> {
+
+    const loginReducer = getLoginReducer(initialState)
+
+    const reducer = combineReducers({
+        login: loginReducer,
+        router: connectRouter(history)
+    })
+
+    const mockStore = createStore(reducer, applyMiddleware(...middlewares));
+    return mockStore
+
+}
+
+export const stubInitialState = {
+    login: { authenticated: true, currentUser: 'Jaeyeon' },
+};
