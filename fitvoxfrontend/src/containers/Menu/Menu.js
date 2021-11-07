44f1a844a2f82bf562ed4738e6a03f7c22f7f09a
tonypark7277 <tonypark7277@naver.com>
2021-11-07 18:05:46 +0900

    temporary

--- fitvoxfrontend/src/containers/Menu/Menu.js
+++ fitvoxfrontend/src/containers/Menu/Menu.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 class Menu extends Component {    
     state = {
-        onToggle: true,
+        onToggle: false,
     }
 
     redirectPersonalSettingHandler = () => {
