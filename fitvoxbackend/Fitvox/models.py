06023928e25e9a895c65fca5b146b0b0df88a19d
jaeyeonkim99 <jaeyeonkim99@naver.com>
2021-11-07 13:33:19 +0900

    Made models

--- fitvoxbackend/Fitvox/models.py
+++ fitvoxbackend/Fitvox/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PersonalSetting(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    hardness = models.CharField(max_length=64)
+    breaktime = models.IntegerField()
+
+
+class ExerciseDefault(models.Model):
+    muscleType = models.CharField(max_length=64)
+    exerciseType = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    hardness = models.TextField()
+
+
+class ExercisePerUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    muscleType = models.CharField(max_length=64)
+    exerciseType = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    hardness = models.TextField()
+    isFavorite = models.BooleanField()
+    volumes = models.JSONField()
+    oneRMs = models.JSONField()
+
+
+
