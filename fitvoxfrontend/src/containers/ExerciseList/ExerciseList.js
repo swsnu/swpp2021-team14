d1791db2ff4b893dfe546d896a3983c237d6bbab
tonypark7277 <tonypark7277@naver.com>
2021-11-10 12:06:18 +0900

    Add search bar on exercise List and add tag filtering

--- fitvoxfrontend/src/containers/ExerciseList/ExerciseList.js
+++ fitvoxfrontend/src/containers/ExerciseList/ExerciseList.js
@@ -14,17 +14,21 @@ class ExerciseList extends Component {
         exerciseType_selected: false,
         muscleType: null,
         exerciseType: null,
-        tags: null
+        tags: [],
+        tag: "#",
+        query: [], 
     }
 
     onMuscleTypeClick(muscleType) {
         this.setState({muscleType_selected: true})
         this.setState({muscleType: muscleType})
+        this.setState({query: [...this.state.query, muscleType]})
     }
 
     onExerciseTypeClick(exerciseType) {
         this.setState({exerciseType_selected: true})
         this.setState({exerciseType: exerciseType})
+        this.setState({query: [...this.state.query, exerciseType]})
     }
 
     onExerciseEntryClick = (name)=>{
@@ -37,11 +41,54 @@ class ExerciseList extends Component {
         this.setState({muscleType: null})
         this.setState({exerciseType_selected: false})
         this.setState({exerciseType: null})
+        this.setState({query: []})
     }
 
     onGoBackExerciseType = () => {
         this.setState({exerciseType_selected: false})
         this.setState({exerciseType: null})
+        this.setState({query: [this.state.query[0]]})
+    }
+
+    onSearchTagHandler = () => {
+        let input_tag = this.state.tag;
+        this.setState({query:[...this.state.query, input_tag], tag: "#", tags:[...this.state.tags, input_tag]})
+    }
+
+    searchBar = () => {
+        let add_tag_button = null;
+        if (this.state.exerciseType_selected){
+            add_tag_button = (<button id="search-with-tag"
+                style = {{width: '5%', height: 100}}
+                onClick = {()=>this.onSearchTagHandler()}>
+                    Add Tag
+            </button>)
+        }
+        return (
+            <div className = "SearchBar" align = "center">
+                <div>
+                    <h1>SearchBar</h1>
+                    <input
+                        id = "search-bar"
+                        value = {this.state.query}
+                        style={{ width: '30%', height: 50 }}
+                        //onChange={(event)=>this.setState({query: [...this.state.query, event.target.value]})}
+                    />
+                </div>
+                <div>
+                    <h1>Type Tag to search</h1>
+                    <div>
+                        <textarea
+                            id = "tag-bar"
+                            value = {this.state.tag}
+                            style={{ width: '30%', height: 100 }}
+                            onChange = {(event)=>this.setState({tag: event.target.value})}
+                        />
+                        {add_tag_button}
+                    </div>
+                </div>
+            </div>
+        )
     }
 
     render() {
@@ -55,6 +102,7 @@ class ExerciseList extends Component {
             }
             return (
                 <div>
+                    {this.searchBar()}
                     <h1>Select Muscle Type</h1>
                     <div>{muscleTypeIcons}</div>
                 </div>
@@ -74,6 +122,7 @@ class ExerciseList extends Component {
 
             return (
                 <div>
+                    {this.searchBar()}
                     <h1>Select Exercise Type</h1>
                     <h2>Selected Muscle Type: {this.state.muscleType}</h2>
                     <div>
@@ -91,26 +140,45 @@ class ExerciseList extends Component {
                     exercise['exerciseType']===this.state.exerciseType)
                     //  && exercise['hardness'].indexOf(this.state.hardness)!==-1
                     {
-                        const name = exercise['name']
-                        exerciseEntries.push(<ExerciseEntry name={name}
-                                                                 onClick={() => this.onExerciseEntryClick(name)}/>)
+                        if (this.state.tags.length === 0){
+                            const name = exercise['name']
+                            exerciseEntries.push(<ExerciseEntry name={name}
+                                                                onClick={() => this.onExerciseEntryClick(name)}/>)
+                        }
+                        else {
+                            let flag = true;
+                            for (let tag of this.state.tags){
+                                if (!exercise["tags"]["tags"].includes(tag)) {
+                                    flag = false;
+                                    break;
+                                }
+                            }
+                            if (flag){
+                                const name = exercise['name']
+                                exerciseEntries.push(<ExerciseEntry name={name}
+                                                                    onClick={() => this.onExerciseEntryClick(name)}/>)
+                            }
+                        }
                     }
                 }
             }
 
             return (
                 <div>
+                    {this.searchBar()}
                     <h2>Selected Muscle Type: {this.state.muscleType}</h2>
                     <h2>Selected Exercise Type: {this.state.exerciseType}</h2>
                     <div>
                         <button onClick={() => this.onGoBackMuscleType()}>Select Muscle Type again</button>
                         <button onClick={()=>this.onGoBackExerciseType()}>Select Exercise Type again</button>
                     </div>
-                    <div>
-                        <h2>Add tags</h2>
-                        <input type="text"/>
-                        <button>Add</button>
-                    </div>
+                    {
+                    //<div>
+                    //    <h2>Add tags</h2>
+                    //    <input type="text"/>
+                    //    <button>Add</button>
+                    //</div>
+                    }
                     <div>
                         {exerciseEntries}
                     </div>
