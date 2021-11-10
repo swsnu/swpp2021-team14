e6a15cfce2cb0941e957e7ff11227fffb445718b
tonypark7277 <tonypark7277@naver.com>
2021-11-11 03:44:07 +0900

    Add images to MuscleTypeIcon

--- fitvoxfrontend/src/components/MuscleTypeIcon/MuscleTypeIcon.js
+++ fitvoxfrontend/src/components/MuscleTypeIcon/MuscleTypeIcon.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-
+import './MuscleTypeIcon.css'
 class MuscleTypeIcon extends Component {
     render() {
         return (
             <div className="MuscleTypeIcon" onClick={() => this.props.onClick()} style={{border: '1px solid red'}}>
-                <h2>{this.props.muscleType}</h2>
+                <div className="iamge"><img src = {'MuscleTypeImage/'+this.props.muscleType+".png"}/></div>
+                <div><h2><span>{this.props.muscleType}</span></h2></div>
+                
             </div>
         );
     }
