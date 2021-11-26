697ac4d8c71c70560400128fa17e32916f892497
jaeyeonkim99 <jaeyeonkim99@naver.com>
2021-11-27 03:16:56 +0900

    Done with exerciselistAction test

---
+++ fitvoxfrontend/src/store/actions/exerciselistActions/exerciselistActions.test.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import * as actionCreators from './exerciselistActions'
+import store from "../../store";
+
+const exerciseList = []
+
+describe('ActionCreator Exercise', () => {
+    afterEach(() => jest.clearAllMocks());
+
+    it('should get exercise list correctly', (done) => {
+        const spyGet = jest.spyOn(axios, 'get').mockImplementation((url) => {
+            return new Promise((resolve, reject) => {
+                const result = {
+                    status: 200,
+                    data: exerciseList
+                }
+                resolve(result)
+            })
+        })
+
+        store.dispatch(actionCreators.getExerciseList())
+        expect(spyGet).toHaveBeenCalledTimes(1)
+        done()
+    })
+
+    it('should add exercise correctly', (done) => {
+
+        const data = {data: "mock data"}
+
+        const spyPost = jest.spyOn(axios, 'post').mockImplementation((url, data) => {
+            return new Promise((resolve, reject) => {
+                const result = {
+                    status: 200
+                }
+                resolve(result)
+            })
+        })
+
+        store.dispatch(actionCreators.addExercise(data));
+        expect(spyPost).toBeCalledWith('/api/exercise_list/', data);
+        done();
+    })
+
+    it('should change tag and check favorite correctly', (done) => {
+        const spyPut = jest.spyOn(axios, 'put').mockImplementation((url, data) => {
+            return new Promise(resolve => {
+                resolve({status: 200, data: exerciseList})
+            })
+        })
+        const favorite = {favorite: true}
+        const tags = {tags:"mock tags"}
+        store.dispatch(actionCreators.checkFavorite(favorite));
+        expect(spyPut).toBeCalledWith('/api/exercise_list/', favorite);
+
+        store.dispatch(actionCreators.changeTags(tags));
+        expect(spyPut).toBeCalledWith('/api/exercise_list/', tags);
+        done();
+    })
+
+});
